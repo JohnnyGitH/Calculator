310003e5ab81fd668d841c127bd221ead3592c64
Jonathan Cockrem <jctotalportal@gmail.com>
2019-02-09 21:58:36 -0500

    Added  Main input functionality. Helps me see what my equation objects will look like. Added CalculatorLibrary project

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,6 +10,68 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+
+            GatheringInput();
+
+
+
+        }
+
+        public static void GatheringInput()
+        {
+            string op;
+            string newOp;
+            double value1;
+            double value2;
+
+            Console.WriteLine("Welcome to my VHS app \n");
+            Console.WriteLine("Type A - for Addition \n");
+            Console.WriteLine("Type B - for Subtraction \n");
+            Console.WriteLine("Type C - for Multiplication \n");
+            Console.WriteLine("Type D - for Division \n");
+            op = Console.ReadLine().ToUpper();
+
+            newOp = DetermineOperation(op);
+
+            Console.WriteLine("Thank you \n");
+            Console.WriteLine("Please enter your first value: ");
+            value1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Thank you \n");
+            Console.WriteLine("Please enter your second value: ");
+            value2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Thank you \n");
+
+            Console.WriteLine("Ok, your equation is " + value1 + newOp + value2);
+        }
+
+        public static string DetermineOperation(string op)
+        {
+            if (op == null)
+            {
+                throw new ArgumentNullException(nameof(op));
+            }
+
+            string opValue;
+
+            switch (op)
+            {
+                case "A":
+                    opValue = " + ";
+                    return opValue;
+                case "B":
+                    opValue = " - ";
+                    return opValue;
+                case "C":
+                    opValue = " * ";
+                    return opValue;
+                case "D":
+                    opValue = " / ";
+                    return opValue;
+
+                default:
+                    throw new FormatException();
+                    
+            }
         }
     }
 }
---
+++ Calculator/CalculatorLibrary/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+
+        // Math based functions/methods should go here eventually
+
+    }
+}
