21565a8af282a915c6d359e054209674700d7b4d
Jonathan Cockrem <jctotalportal@gmail.com>
2019-02-10 00:41:10 -0500

    Created Equation model with accessor methods. Also added testing projects

--- Calculator/Calculator/Models/Equation.cs
+++ Calculator/Calculator/Models/Equation.cs
@@ -6,8 +6,60 @@ using System.Threading.Tasks;
 
 namespace Calculator.Equation
 {
-    class Equation
+    public class Equation
     {
-        
+        public string op { get; set; }
+        public string newOperation { get; set; }
+        public double value1 { get; set; }
+        public double value2 { get; set; }
+
+
+        public double GetValue1(Equation eq)
+        {
+            if (eq == null)
+            {
+                throw new ArgumentNullException(nameof(eq));
+            }
+
+            double value = eq.value1;
+
+            return value;
+        }
+
+        public double GetValue2(Equation eq)
+        {
+            if (eq == null)
+            {
+                throw new ArgumentNullException(nameof(eq));
+            }
+
+            double value = eq.value2;
+
+            return value;
+        }
+
+        public string GetOperation(Equation eq)
+        {
+            if (eq == null)
+            {
+                throw new ArgumentNullException(nameof(eq));
+            }
+
+            string value = eq.op;
+
+            return value;
+        }
+
+        public string GetNewOperation(Equation eq)
+        {
+            if (eq == null)
+            {
+                throw new ArgumentNullException(nameof(eq));
+            }
+
+            string value = eq.newOperation;
+
+            return value;
+        }
     }
 }
